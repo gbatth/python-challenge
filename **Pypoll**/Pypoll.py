919edcc4d610df306e373e4f4dc66f84e28a7773
gbatth <69115310+gbatth@users.noreply.github.com>
2020-09-05 16:56:18 -0700

    Update Pypoll.py

--- **Pypoll**/Pypoll.py
+++ **Pypoll**/Pypoll.py
@@ -5,10 +5,9 @@ import csv
 file = os.path.join("Resources", "election_data.csv")
 
 poll = {}
-num_votes =[]
-total_votes = 0
+num_of_votes =[]
+tot_votes = 0
 candidates = []
-csv_reader = ['1','2']
 vote_Percent = []
 winner_list = []
 
@@ -18,7 +17,7 @@ with open(file,'r') as csvfile:
     next(csvreader)
     
     for row in csvreader:
-        total_votes += 1
+        tot_votes = tot_votes + 1
         if row[2] in poll.keys():
             poll[row[2]] = poll[row[2]] + 1
         else:
@@ -26,15 +25,15 @@ with open(file,'r') as csvfile:
 
 for key, value in poll.items():
     candidates.append(key)
-    num_votes.append(value)
+    num_of_votes.append(value)
 
-for n in num_votes:
-    vote_Percent.append(round(n/total_votes*100, 1))
+for n in num_of_votes:
+    vote_Percent.append(round(n/tot_votes*100, 1))
 
-    clean_data = list(zip(candidates,num_votes,vote_Percent))
+    clean_data = list(zip(candidates,num_of_votes,vote_Percent))
 
 for name in clean_data:
-    if max(num_votes) == name[1]:
+    if max(num_of_votes) == name[1]:
         winner_list.append(name[0])
 
 winner = winner_list[0]
@@ -47,7 +46,7 @@ if len(winner_list) > 1:
 output_file = os.path.join('election_results' + 'txt')
 
 with open(output_file, 'w') as txtfile:
-    txtfile.writelines('election Results \n----------------------------------- \nTotal Votes: ' + str(total_votes) +'\n--------------------------------\n')
+    txtfile.writelines('election Results \n----------------------------------- \nTot Votes: ' + str(tot_votes) +'\n--------------------------------\n')
     for entry in clean_data:
         txtfile.writelines(entry[0] + ":" + str(entry[2]) + '% (' + str(entry[1]) + ')\n')
     txtfile.writelines('------------------------ \nWinner: ' + winner + '\n-----------------------')
